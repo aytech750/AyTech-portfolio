54a268934fe3061f51395d6b0d79faa197edd611
aytech750 <ayorixzfx@gmail.com>
2025-10-04 03:42:41 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // global styles
-import './app.css';   // homepage-specific styles
+import './App.css';   // homepage-specific styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
